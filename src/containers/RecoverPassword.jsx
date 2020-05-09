2c709b14222447966444dccf2df17bc4c1518883
Oche Moses <ocmoses@yahoo.com>
2020-05-09 02:43:18 +0100

    I remember my credentials, sign in

--- src/containers/RecoverPassword.jsx
+++ src/containers/RecoverPassword.jsx
@@ -259,7 +259,7 @@ const RecoverPassword = () => {
                       display: "inline",
                     }}
                   >
-                    Sign In instead
+                    Sign In
                   </Link>
                 </p>
               </Grid>
